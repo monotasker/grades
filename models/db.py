75917b5f9f3507095d9bd45ce9724ffaa947779a
Ian Scott <scottianw@gmail.com>
2012-09-25 14:34:22 -0400

    Latest version of peergrades

--- models/db.py
+++ models/db.py
@@ -5,18 +5,7 @@
 ## File is released under public domain and you can use without limitations
 #########################################################################
 
-if not request.env.web2py_runtime_gae:     
-    ## if NOT running on Google App Engine use SQLite or other DB
-    db = DAL('sqlite://storage.sqlite') 
-else:
-    ## connect to Google BigTable (optional 'google:datastore://namespace')
-    db = DAL('google:datastore') 
-    ## store sessions and tickets there
-    session.connect(request, response, db = db) 
-    ## or store session in Memcache, Redis, etc.
-    ## from gluon.contrib.memdb import MEMDB
-    ## from google.appengine.api.memcache import Client
-    ## session.connect(request, response, db = MEMDB(Client()))
+db = DAL('sqlite://storage.sqlite') 
 
 ## by default give a view/generic.extension to all actions from localhost
 ## none otherwise. a pattern can be 'controller/function.extension'
@@ -33,51 +22,31 @@ response.generic_patterns = ['*'] if request.is_local else []
 #########################################################################
 
 from gluon.tools import Auth, Crud, Service, PluginManager, prettydate
+from gluon.tools import Mail, Recaptcha
 auth = Auth(db, hmac_key=Auth.get_or_create_key()) 
 crud, service, plugins = Crud(db), Service(), PluginManager()
 
 ## create all tables needed by auth if not custom tables
 auth.define_tables()
 
+keydata = {}
+with open('applications/peergrades/private/app.keys', 'r') as keyfile:
+    for line in keyfile:
+        k, v = line.split()
+	keydata[k] = v
+
 ## configure email
-mail=auth.settings.mailer
-mail.settings.server = 'logging' or 'smtp.gmail.com:587'
-mail.settings.sender = 'scottianw@gmail.com'
-mail.settings.login = 'username:password'
+mail=Mail()
+mail.settings.server = keydata['email_sender'] or 'smtp.gmail.com:587'
+mail.settings.sender = keydata['email_address']
+mail.settings.login = keydata['email_pass']
 
 ## configure auth policy
 auth.settings.registration_requires_verification = False
 auth.settings.registration_requires_approval = False
 auth.settings.reset_password_requires_verification = True
 
-from gluon.tools import Recaptcha
 auth.settings.login_captcha = None
-auth.settings.register_captcha = Recaptcha(request, '6Ldy5ccSAAAAALlI2gJuwFQYe-iaZ_oyQs3nhX-9', '6Ldy5ccSAAAAABr8FhJeb_aELfEC7SJOOyvhJp0R')
-auth.settings.retrieve_username_captcha = Recaptcha(request, '6Ldy5ccSAAAAALlI2gJuwFQYe-iaZ_oyQs3nhX-9', '6Ldy5ccSAAAAABr8FhJeb_aELfEC7SJOOyvhJp0R')
-auth.settings.retrieve_password_captcha = Recaptcha(request, '6Ldy5ccSAAAAALlI2gJuwFQYe-iaZ_oyQs3nhX-9', '6Ldy5ccSAAAAABr8FhJeb_aELfEC7SJOOyvhJp0R')
-
-## if you need to use OpenID, Facebook, MySpace, Twitter, Linkedin, etc.
-## register with janrain.com, write your domain:api_key in private/janrain.key
-from gluon.contrib.login_methods.rpx_account import use_janrain
-use_janrain(auth,filename='private/janrain.key')
-
-#########################################################################
-## Define your tables below (or better in another model file) for example
-##
-## >>> db.define_table('mytable',Field('myfield','string'))
-##
-## Fields can be 'string','text','password','integer','double','boolean'
-##       'date','time','datetime','blob','upload', 'reference TABLENAME'
-## There is an implicit 'id integer autoincrement' field
-## Consult manual for more options, validators, etc.
-##
-## More API examples for controllers:
-##
-## >>> db.mytable.insert(myfield='value')
-## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
-## >>> for row in rows: print row.id, row.myfield
-#########################################################################
-
-mail.settings.server = settings.email_server
-mail.settings.sender = settings.email_sender
-mail.settings.login = settings.email_login
+auth.settings.register_captcha = Recaptcha(request, keydata['captcha_public_key'], keydata['captcha_private_key'])
+auth.settings.retrieve_username_captcha = Recaptcha(request, keydata['captcha_public_key'], keydata['captcha_private_key'])
+auth.settings.retrieve_password_captcha = Recaptcha(request, keydata['captcha_public_key'], keydata['captcha_private_key'])
